00ddb2b4b9b9063ebde4001821dd959624f39f2d
Josh Cosson <joshlcosson@gmail.com>
2019-12-02 00:50:44 -0800

    First version of burger application. Added logic to display burgers in database and to add a new burger

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Cheeseburger', true);
