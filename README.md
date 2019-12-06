16f2c52a95a19ee8fea435b9cd60a67bdb325957
Josh Cosson <joshlcosson@gmail.com>
2019-12-05 23:13:31 -0800

    Updated connection.js and README.md file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # burger
+
+Eat-Da-Burger! is a restaurant app that lets users log the names of burgers they'd like to eat. Whenever a user submits a burger's name, the app displays the burger on the left side of the page, waiting to be devoured.
+
+Each burger in the waiting area also has a Devour it! button. When the user clicks on 'Devour', the burger will move to the right side of the page.
+
+Once devoured, the user also has the ability to delete the devoured burger from the list and database. If not deleted, the database stores both devoured and non-devoured burgers.
+
+**GIF of application in use**
+
+- Running the application and navigating to survey
+  ![](burger.gif)
+
+**Technologies Used**
+
+- HTML/CSS
+- javascript
+- Express
+- Node server
+- Handlebars
+- mySQL
+- Deployed on Heruku
+
+**Link to the Application, Hosted on Heroku**
+https://infinite-shore-50342.herokuapp.com/
+
+**Credits**
+Developer and app creator: Josh Cosson
--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3307,
-//   user: "root",
-//   password: "IcedMocha4",
-//   database: "burgers_db"
-// });
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "IcedMocha4",
+  database: "burgers_db"
+});
 
 // var connection = mysql.createConnection({
 //   host: "us-cdbr-iron-east-05.cleardb.net",
@@ -16,9 +16,9 @@ var mysql = require("mysql");
 //   password: "a1c90610",
 //   database: "burgers_db"
 // });
-var connection = mysql.createConnection(
-  "mysql://b567962c81a386:a1c90610@us-cdbr-iron-east-05.cleardb.net/heroku_d78b89924f508fc?reconnect=true"
-);
+// var connection = mysql.createConnection(
+//   "mysql://b567962c81a386:a1c90610@us-cdbr-iron-east-05.cleardb.net/heroku_d78b89924f508fc?reconnect=true"
+// );
 // Make connection.
 connection.connect(function(err) {
   if (err) {
