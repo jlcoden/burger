c5d8df36c23e5f8a27e3151f8c9c2a1edf02a06e
Josh Cosson <joshlcosson@gmail.com>
2020-01-02 22:57:20 -0800

    Updated connection.js so that app can connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "IcedMocha4",
-  database: "burgers_db"
-});
+if (process.env.JAWSB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "IcedMocha4",
+    database: "burgers_db"
+  });
+}
 
-// var connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-05.cleardb.net",
-//   port: 3307,
-//   user: "b567962c81a386",
-//   password: "a1c90610",
-//   database: "burgers_db"
-// });
-// var connection = mysql.createConnection(
-//   "mysql://b567962c81a386:a1c90610@us-cdbr-iron-east-05.cleardb.net/heroku_d78b89924f508fc?reconnect=true"
-// );
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
